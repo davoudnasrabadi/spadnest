122e458937ee0acdfaf2e5712901a516e225e7d8
davoudnasrabadi <davoudnsr90@gmail.com>
2022-09-03 21:07:11 +0430

    auth controller added

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,58 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Post,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import { RegisterDto } from './dto/register.dto';
+import { JwtAuthGuard } from './jwt-auth.guard';
+import { TransformPasswordPipe } from './hash-password.pipe';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  /**
+   * Constructor
+   * @param authService
+   */
+  constructor(private authService: AuthService) {}
+
+  /**
+   * Register controller
+   * @param dto
+   * @returns
+   */
+  @UsePipes(ValidationPipe, TransformPasswordPipe)
+  @HttpCode(200)
+  @Post('register')
+  async register(@Body() dto: RegisterDto) {
+    return await this.authService.register(dto);
+  }
+
+  /**
+   * Login Controller
+   * @param dto
+   * @returns
+   */
+  @HttpCode(200)
+  @Post('login')
+  async login(@Body() dto: LoginDto) {
+    return await this.authService.login(dto);
+  }
+
+  /**
+   * Get detail User
+   */
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  async profile() {
+    return {
+      message: 'Profile',
+    };
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,83 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from '../prisma/prisma.service';
+import { LoginDto } from './dto/login.dto';
+import { omit } from 'lodash';
+import { compare } from 'bcrypt';
+import { JwtConfig } from '../jwt.config';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private jwtService: JwtService,
+    private dbService: PrismaService,
+  ) {}
+
+  async register(dto: any) {
+    let user = await this.dbService.users.findFirst({
+      where: {
+        email: dto.email,
+      },
+    });
+    if (user) {
+      throw new HttpException('User Exists', HttpStatus.BAD_REQUEST);
+    }
+    let createUser = await this.dbService.users.create({
+      data: dto,
+    });
+    if (createUser) {
+      return {
+        statusCode: 200,
+        message: 'Register success',
+      };
+    }
+    throw new HttpException('Bad request', HttpStatus.BAD_REQUEST);
+  }
+
+  async login(dto: LoginDto) {
+    let user = await this.dbService.users.findFirst({
+      where: { email: dto.email },
+    });
+
+    if (!user) {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    }
+
+    let checkPassword = await compare(dto.password, user.password);
+    if (!checkPassword) {
+      throw new HttpException('Credential Incorrect', HttpStatus.UNAUTHORIZED);
+    }
+    return await this.generateJwt(
+      user.id,
+      user.email,
+      user,
+      JwtConfig.user_secret,
+      JwtConfig.user_expired,
+    );
+  }
+
+  async generateJwt(
+    userId: any,
+    email: string,
+    user: any,
+    secret: any,
+    expired = JwtConfig.user_expired,
+  ) {
+    let accessToken = await this.jwtService.sign(
+      {
+        sub: userId,
+        email,
+        name: user.first_name + ' ' + user.last_name,
+      },
+      {
+        expiresIn: expired,
+        secret,
+      },
+    );
+    return {
+      statusCode: 200,
+      accessToken: accessToken,
+      user: omit(user, ['password']),
+    };
+  }
+}
