3501df0589aadd904e90b4fcfa9ba1d54050e0bd
davoudnasrabadi <davoudnsr90@gmail.com>
2022-09-03 19:40:41 +0430

    strategy for auth added

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { JwtConfig } from 'src/jwt.config';
+import { JwtConfig } from '../jwt.config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt-user') {
