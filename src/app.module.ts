8d7461e8c0269713e88dbb96d39bacf2f432888b
davoudnasrabadi <davoudnsr90@gmail.com>
2022-09-03 19:17:49 +0430

    prisma init done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { XModule } from './x/x.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, XModule],
   controllers: [AppController],
   providers: [AppService],
 })
