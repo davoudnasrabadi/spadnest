584c65cf476410b8ceb87a0f0522b3398be708d8
davoudnasrabadi <davoudnsr90@gmail.com>
2022-09-03 19:19:45 +0430

    auth module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { XModule } from './x/x.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, XModule],
+  imports: [PrismaModule, XModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
